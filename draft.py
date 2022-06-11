4a7ed564d70043487c890fcada3337da5759a2e7
Darkduv <40823426+Darkduv@users.noreply.github.com>
2022-06-11 21:42:28 +0200

    Légère re-factorisation

--- draft.py
+++ draft.py
@@ -24,16 +24,16 @@ def create_board():
 
 
 def drop_piece(board, row, col, piece):
-    board[row][col] = piece
+    board[row, col] = piece
 
 
 def is_valid_location(board, col):
-    return board[ROW_COUNT - 1][col] == 0
+    return board[ROW_COUNT - 1, col] == 0
 
 
 def get_next_open_row(board, col):
     for r in range(ROW_COUNT):
-        if board[r][col] == 0:
+        if board[r, col] == 0:
             return r
 
 
@@ -45,29 +45,27 @@ def winning_move(board, piece):
     # Check horizontal locations for win
     for c in range(COLUMN_COUNT - 3):
         for r in range(ROW_COUNT):
-            if board[r][c] == piece and board[r][c + 1] == piece and board[r][c + 2] == piece and \
-                    board[r][c + 3] == piece:
+            if (board[r, c: c+4] == piece).all():
                 return True
 
     # Check vertical locations for win
     for c in range(COLUMN_COUNT):
         for r in range(ROW_COUNT - 3):
-            if board[r][c] == piece and board[r + 1][c] == piece and board[r + 2][c] == piece and \
-                    board[r + 3][c] == piece:
+            if (board[r: r+4, c] == piece).all():
                 return True
 
     # Check positively sloped diagonals
     for c in range(COLUMN_COUNT - 3):
         for r in range(ROW_COUNT - 3):
-            if board[r][c] == piece and board[r + 1][c + 1] == piece and \
-                    board[r + 2][c + 2] == piece and board[r + 3][c + 3] == piece:
+            if board[r, c] == piece and board[r + 1, c + 1] == piece and \
+                    board[r + 2, c + 2] == piece and board[r + 3, c + 3] == piece:
                 return True
 
     # Check negatively sloped diagonals
     for c in range(COLUMN_COUNT - 3):
         for r in range(3, ROW_COUNT):
-            if board[r][c] == piece and board[r - 1][c + 1] == piece and \
-                    board[r - 2][c + 2] == piece and board[r - 3][c + 3] == piece:
+            if board[r, c] == piece and board[r - 1, c + 1] == piece and \
+                    board[r - 2, c + 2] == piece and board[r - 3, c + 3] == piece:
                 return True
 
 
@@ -82,7 +80,7 @@ def draw_board(board):
 
     for c in range(COLUMN_COUNT):
         for r in range(ROW_COUNT):
-            player_ = board[r][c]
+            player_ = board[r, c]
             if player_ > 0:
                 col_player = COL_PLAYERS[player_]
                 pygame.draw.circle(screen, col_player, (int(c * SQUARESIZE + SQUARESIZE / 2),
@@ -122,10 +120,8 @@ while not game_over:
         if event.type == pygame.MOUSEMOTION:
             pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
             posx = event.pos[0]
-            if turn == 0:
-                pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE / 2)), RADIUS)
-            else:
-                pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE / 2)), RADIUS)
+            pygame.draw.circle(screen, COL_PLAYERS[turn + 1],
+                               (posx, int(SQUARESIZE / 2)), RADIUS)
         pygame.display.update()
 
         if event.type == pygame.MOUSEBUTTONDOWN:
