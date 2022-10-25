9156a6480b786b58f601cff5e6f91ea03c1adb47
Darkduv <40823426+Darkduv@users.noreply.github.com>
2022-10-25 21:16:25 +0200

    start better implementation, splitting 'game theory' and real gaming method, and the GUI manager

--- main.py
+++ main.py
@@ -12,6 +12,7 @@
 """
 import tkinter
 from tkinter import Frame, Canvas, Toplevel, Scale, Message
+from abc import ABC, abstractmethod
 from super_matrix import SuperMatrix
 
 
@@ -22,6 +23,89 @@ from super_matrix import SuperMatrix
 # Todo 2: a better way to code the search of a victory ??
 
 # Todo 1: keyboard shortcuts like cmd + U for Undo, etc...  -> Partly done
+
+class EmptyHistoricError(Exception):
+    """Raise when trying accessing a value of an empty historic"""
+
+
+EmptyHistoric = EmptyHistoricError()
+
+
+class SimpleHistoric:
+    """For keeping track of the actions"""
+
+    def __init__(self, l_saves: list = None, current_undo: list = None):
+        if l_saves is None:
+            l_saves = []
+        if current_undo is None:
+            current_undo = []
+        self.l_saves = l_saves
+        self.current_undo = current_undo
+
+    def save_new(self, save):
+        """Save a state or an action"""
+        self.l_saves.append(save)
+        self.current_undo = []
+
+    def undo(self):
+        if not self.l_saves:
+            raise EmptyHistoric
+        save = self.l_saves.pop(-1)
+        self.current_undo.append(save)
+        return save
+
+    def redo(self):
+        if not self.current_undo:
+            raise EmptyHistoric
+        save = self.current_undo.pop(-1)
+        self.l_saves.append(save)
+        return save
+
+
+class GameNPlayer(ABC):
+    """Skeleton of a game of N players"""
+
+    def __init__(self, nb_players=2):
+        self.nb_players = nb_players
+        self.player = 0  # player currently playing
+        self.turn = 0  # nb round/turn
+
+    @abstractmethod
+    def play(self, action) -> bool:
+        """Must implement the given action. Return if the player wins."""
+
+    @abstractmethod
+    def win(self, action) -> int:
+        """if winning, return the id of the player winning. else -1"""
+
+    def next_player(self):
+        """Update new id player"""
+        self.player += 1
+        self.player %= self.nb_players
+
+    def undo_action(self, action):
+        """ Undo the action """
+        raise NotImplemented
+
+    def redo_action(self, action_or_state):
+        """ Redo the action """
+        raise NotImplemented
+
+    def apply(self, state_saved):
+        """Set game to the given state"""
+        raise NotImplemented
+
+    @abstractmethod
+    def init_game(self, *args, **kwargs):
+        """ (Re) initialize the game """
+
+    def copy(self) -> "GameNPlayer":
+        """ Copy the object """
+        raise NotImplemented
+
+    def export_save(self):
+        """ Export/Save the game in a lighter way than copying"""
+        raise NotImplemented
 
 
 class MenuBar(tkinter.Menu):
