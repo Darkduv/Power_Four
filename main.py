88fc0f2b227264fcde1cedb0209044ce9e6f2216
Darkduv <40823426+Darkduv@users.noreply.github.com>
2022-10-26 11:48:04 +0200

    split game mechanics and gui

--- main.py
+++ main.py
@@ -28,7 +28,12 @@ class EmptyHistoricError(Exception):
     """Raise when trying accessing a value of an empty historic"""
 
 
+class InvalidActionError(Exception):
+    """Raise when trying to make an invalid action"""
+
+
 EmptyHistoric = EmptyHistoricError()
+InvalidAction = InvalidActionError()
 
 
 class SimpleHistoric:
@@ -73,6 +78,14 @@ class GameNPlayer(ABC):
     @abstractmethod
     def play(self, action) -> bool:
         """Must implement the given action. Return if the player wins."""
+
+    @abstractmethod
+    def can_play(self) -> bool:
+        """Can the current player play ?"""
+
+    @abstractmethod
+    def possible_actions(self):
+        """Get the possibles actions for the current player"""
 
     @abstractmethod
     def win(self, action) -> int:
@@ -136,8 +149,106 @@ class MenuBar(tkinter.Menu):
                 parent.add_command(label=label, command=menu_command)
 
 
-class Panel(Frame):
-    """Panel de jeu (grille de n x m cases)"""
+class PowerFour(GameNPlayer):
+    _nb_players = 2
+    _not_a_player = -1
+
+    def __init__(self, n_rows: int = 6, n_cols: int = 7):
+        super().__init__(self._nb_players)
+        self.n_rows = n_rows
+        self.n_cols = n_cols
+        self.grid = SuperMatrix(self._not_a_player, n_rows, n_cols)
+        self.init_game()
+
+    def init_game(self, *args, **kwargs):
+        self.grid = SuperMatrix(-1, self.n_rows, self.n_cols)
+        self.turn = 0
+        self.player = 0
+
+    def play(self, col: int) -> int:
+        if 0 > col or self.n_cols <= col:
+            raise InvalidActionError("Col not in the good range")
+        row = 0
+        while row < self.n_rows \
+                and self.grid[row][col] == self._not_a_player:
+            row += 1
+        row -= 1
+        if row == -1:
+            raise InvalidActionError("Column is filled")
+        self.grid[row][col] = self.player
+        win = self.win(col)  # todo : efficient winning test
+
+        self.next_player()
+        return win
+
+    def win(self, action) -> int:
+        r"""Give the id of the winning player, -1 if no victory.
+        /!\ Here the test is only on the current player"""
+        # todo : efficient winning test
+        player = self.player
+        alignments = self.search_alignment(player)
+        return player if alignments else -1
+        
+    def search_alignment(self, player: int = None):
+        orientation = [[0, 1], [1, 0], [1, 1], [1, -1]]
+        if player is None:
+            player = self.player
+        all_alignments = []
+        for row in range(self.n_rows):
+            for col in range(self.n_cols):
+                if self.grid[row][col] != player:
+                    continue
+                for sens in orientation:
+                    x2, y2 = row, col
+                    alignment = [[row, col]]
+                    for _ in range(3):
+                        x2 += sens[0]
+                        y2 += sens[1]
+                        if not (0 <= x2 < self.n_rows
+                                and 0 <= y2 < self.n_cols):
+                            break
+                        alignment.append([x2, y2])
+                        if self.grid[x2][y2] != player:
+                            break
+                    else:
+                        # add alignment
+                        all_alignments.append(alignment)
+        return all_alignments
+
+    def __str__(self):
+        s = ""
+        s += f"Power 4, turn={self.turn}, player={self.player}\n"
+        s += "="*(2*self.n_cols + 1)+"\n"
+        for row in range(self.n_rows):
+            s += f"|{' '.join('.OX'[player+1] for player in self.grid[row])}|\n"
+        s += "="*(2*self.n_cols + 1)
+        return s
+    
+    def can_play(self) -> bool:
+        for col in range(self.n_cols):
+            if self.grid[0][col] != self._not_a_player:
+                return True
+        return False
+
+    def possible_actions(self):
+        actions = []
+        for col in range(self.n_cols):
+            if self.grid[0][col] != self._not_a_player:
+                actions.append(col)
+        return actions
+
+    def copy(self) -> "PowerFour":
+        copy = PowerFour(n_rows=self.n_rows, n_cols=self.n_cols)
+        copy.grid = [row[:] for row in self.grid]
+        copy.turn = self.turn
+        copy.player = self.player
+        return copy
+
+
+class GridGUI(Frame):
+    """GUI for the Grid of Power4
+    TODO : force first player color ?
+            #  (be careful with A.I. definition)"""
 
     def __init__(self, root):
         # Red = 0 and Yellow = 1
@@ -147,7 +258,6 @@ class Panel(Frame):
         # cases (squared) of the grid, et the dimensions of the
         # canvas are adapted in consequence.
         super().__init__(root)
-        self.n_row, self.n_col = 6, 7  # initial grid = 6*7
         # Link of the event <resize> with an adapted manager :
         self.bind("<Configure>", self.rescale)
         # Canvas :
@@ -171,37 +281,29 @@ class Panel(Frame):
                                                  width=1, fill="yellow")
         # self.can_bis = Label(text="Red's\n turn")
         self.can_bis.pack(side=tkinter.RIGHT, expand=tkinter.YES)
-        self.player = 1  # TODO = to change color of first player.
         # construction of a list of lists
-        self.state = SuperMatrix(2, self.n_row, self.n_col)
-        self.game = []
-        self.history = []
-        self.coup = 0
+        self.grid = PowerFour(6, 7)  # default values : 6*7
+        self.history = SimpleHistoric()
         self.width, self.height = 2, 2
         self.cote = 0
         self.win = False
         self.init_jeu()
 
-    def init_jeu(self, state=None):
+    def init_jeu(self, state: PowerFour = None):
         """Initialisation of the list which remember the state of the game"""
         self.win = False
         if state is None:
-            # TODO : here to force first player color ?
-            #  (be careful with A.I. definition)
-            # self.player = 1
-            self.state = SuperMatrix(2, self.n_row, self.n_col)
-            self.game.append(self.state)
+            self.grid.init_game()
         else:
-            self.state = state.copy()
-        self.game.append(self.state)
+            self.grid = state
         self.set_side()
         self.trace_grille()
 
     def set_side(self):
         """Set the side value in relation with the window size"""
         # maximal width and height possibles for the cases :
-        l_max = self.width / self.n_col
-        h_max = self.height / self.n_row
+        l_max = self.width / self.grid.n_cols
+        h_max = self.height / self.grid.n_rows
         # the side of a case would be the smallest of the two dimensions :
         self.cote = min(l_max, h_max)
 
@@ -218,29 +320,30 @@ class Panel(Frame):
     def trace_grille(self):
         """Layout of the grid, in function of dimensions and options"""
         # -> establishment of new dimensions for the canvas :
-        wide, high = self.cote * self.n_col, self.cote * self.n_row
+        wide, high = self.cote * self.grid.n_cols, self.cote * self.grid.n_rows
         self.can.configure(width=wide, height=high)
         # Layout of the grid:
         self.can.delete(tkinter.ALL)  # erasing of the past Layouts
         # Layout of all the pawns,
         # white or black according to the state of the game :
-        for l in range(self.n_row):
-            for c in range(self.n_col):
+        for l in range(self.grid.n_rows):
+            for c in range(self.grid.n_cols):
                 x1 = c * self.cote + 3  # size of pawns =
                 x2 = (c + 1) * self.cote - 3  # size of the case -6
                 y1 = l * self.cote + 3  #
                 y2 = (l + 1) * self.cote - 3
-                color = ["red", "yellow", "white", "black"][self.state[l][c]]
+                color = ["white", "red", "yellow", "black"][self.grid.grid[l][c]+1]
                 self.can.create_oval(x1, y1, x2, y2, outline="grey",
                                      width=1, fill=color)
         self.can_bis.configure(width=self.width - wide, height=self.height)
         self.can_bis.delete(self.turn, self.turn_bis)
         self.turn = \
-            self.can_bis.create_text(self.can_bis.winfo_width() / 2,
-                                     self.can_bis.winfo_height() / 3,
-                                     text=f"{['Red', 'Yellow'][self.player]}'s"
-                                          f"\n turn",
-                                     font="Helvetica 18 bold")
+            self.can_bis.create_text(
+                self.can_bis.winfo_width() / 2,
+                self.can_bis.winfo_height() / 3,
+                text=f"{['Red', 'Yellow'][self.grid.player]}'s"
+                     f"\n turn",
+                font="Helvetica 18 bold")
         x = self.can_bis.winfo_width() / 3
         y = self.can_bis.winfo_height() / 3
         r = min([x, y, 40])
@@ -250,129 +353,92 @@ class Panel(Frame):
         y2 = y * 2 + r
         self.turn_bis\
             = self.can_bis.create_oval(x1, y1, x2, y2, outline="grey", width=1,
-                                       fill=["red", "yellow"][self.player])
+                                       fill=["red", "yellow"][self.grid.player])
 
     def click(self, event):
         """Management of the mouse click : return the pawns"""
         # We start to determinate the line and the columns :
-        row, col = int(event.y / self.cote), int(event.x / self.cote)
-        if 0 <= row < self.n_row and 0 <= col < self.n_col:
-            # maximal width and height possibles for the cases :
-            l_max = self.width / self.n_col
-            h_max = self.height / self.n_row
-            # the side of a case would be the smallest of the two dimensions :
-            self.cote = min(l_max, h_max)
-            wide, high = self.cote * self.n_col, self.cote * self.n_row
-            self.can.configure(width=wide, height=high)
-            n = 0
-            x1 = self.cote * col + 3
-            x2 = self.cote * (col + 1) - 3
-            while n < self.n_row and self.state[n][col] == 2:
-                y1 = n * self.cote + 3
-                y2 = (n + 1) * self.cote - 3
-                color = ["red", "yellow"][self.player]
-                self.can.create_oval(x1, y1 - self.cote, x2, y2 - self.cote,
-                                     outline="grey", width=1, fill="white")
-                self.update()
-                self.can.create_oval(x1, y1, x2, y2, outline="grey",
-                                     width=1, fill=color)
-                self.update()
-                self.can.after(100)
-                n += 1
+        # row, int(event.y / self.cote),
+        if self.win:
+            return
+        col = int(event.x / self.cote)
+        if not 0 <= col < self.grid.n_cols:
+            raise InvalidActionError(
+                "Click in the grid to make a valid action")
+            # return
 
-            if 1 <= n <= self.n_row:
-                self.state[n - 1][col] = self.player
-                self.history.append([n-1, col, self.player])
-                self.coup += 1
-                if not self.display_victory():
-                    try:
-                        self.game[self.coup] = self.state.copy()
-                    except IndexError:
-                        self.game.append(self.state.copy())
-                    self.player += 1
-                    self.player %= 2
-                    # self.can_bis.destroy()
-                    # self.can_bis = Label(text=["Red", "Yellow"][self.player]+\
-                    # "'s\n turn", font="Helvetica 15 bold")
-                    self.can_bis.itemconfig(
-                        self.turn,
-                        text=f"{['Red', 'Yellow'][self.player]}'s\n turn")
-                    self.can_bis.itemconfig(
-                        self.turn_bis, fill=["red", "yellow"][self.player])
+        player = self.grid.player
+        self.history.save_new(self.grid.copy())
+        win = self.grid.play(col)
+        # maximal width and height possibles for the cases :
+        self.set_side()
+        wide, high = self.cote * self.grid.n_cols, self.cote * self.grid.n_rows
+        self.can.configure(width=wide, height=high)
+        row = 0
+        x1 = self.cote * col + 3
+        x2 = self.cote * (col + 1) - 3
+        color_player = ["red", "yellow"][player]
+        while row < self.grid.n_rows:
+            y1 = row * self.cote + 3
+            y2 = (row + 1) * self.cote - 3
+            self.can.create_oval(x1, y1 - self.cote, x2, y2 - self.cote,
+                                 outline="grey", width=1, fill="white")
+            self.update()
+            self.can.create_oval(x1, y1, x2, y2, outline="grey",
+                                 width=1, fill=color_player)
+            self.update()
+            self.can.after(100)
+            if self.grid.grid[row][col] != -1:
+                break
+            row += 1
 
-                    # print("hello")
-                    self.can_bis.pack(side=tkinter.RIGHT)
-                    self.can_bis.update()
-                    self.trace_grille()
-                else:
-                    self.can_bis.itemconfig(
-                        self.turn,
-                        text=f"{['Red', 'Yellow'][self.player]}\n wins !!")
-                    self.can_bis.delete(self.turn_bis)
-                    try:
-                        self.game[self.coup] = self.state.copy()
-                    except IndexError:
-                        self.game.append(self.state.copy())
-                    self.player += 1
-                    self.player %= 2
+        if win == -1:
+            # self.can_bis.destroy()
+            # self.can_bis = Label(text=["Red", "Yellow"][self.player]+\
+            # "'s\n turn", font="Helvetica 15 bold")
+            self.can_bis.itemconfig(
+                self.turn,
+                text=f"{['Red', 'Yellow'][self.grid.player]}'s\n turn")
+            self.can_bis.itemconfig(
+                self.turn_bis, fill=["red", "yellow"][self.grid.player])
+
+            self.can_bis.pack(side=tkinter.RIGHT)
+            self.can_bis.update()
+            self.trace_grille()
+        else:
+            self.win = True
+            self.display_victory(player)
+            self.update()
+
+            self.can_bis.itemconfig(
+                self.turn,
+                text=f"{['Red', 'Yellow'][player]}\n wins !!")
+            self.can_bis.delete(self.turn_bis)
 
     def undo(self, event=None):
-        if self.coup > 0:
-            self.coup -= 1
-            self.player += 1
-            self.player %= 2
-            self.init_jeu(self.game[self.coup].copy())
+        last_state = self.history.undo()
+        self.win = False
+        self.grid = last_state
         if event:
             pass
+        self.trace_grille()
 
     def reset(self, event=None):
-        """  french!  """
         self.init_jeu()
-        self.coup = 0
+        self.win = False
         self.trace_grille()
         if event:
             pass
 
-    def victory_threaten(self):
-        # Todo : threaten.... ??
-        orientation = [[0, 1], [1, 0], [1, 1], [-1, 1]]
-        color = self.player
-        all_alignments = []
-        for x in range(self.n_row):
-            for y in range(self.n_col):
-                if self.state[x][y] != color:
-                    continue
-                for sens in orientation:
-                    x2, y2 = x, y
-                    victory = True
-                    alignment = [[x, y]]
-                    for _ in range(3):
-                        x2 += sens[0]
-                        y2 += sens[1]
-                        alignment.append([x2, y2])
-                        try:
-                            if x2 < 0 or y2 < 0 or self.state[x2][y2] != color:
-                                victory = False
-                                break
-                        except IndexError:
-                            victory = False
-                            break
-
-                    if victory:
-                        # return alignment
-                        all_alignments.append(alignment)
-        return all_alignments
-
-    def display_victory(self):
-        all_alignment = self.victory_threaten()
+    def display_victory(self, player=None):
+        all_alignment = self.grid.search_alignment(player)
         if not all_alignment:
             return False
-        else:
-            for alignment in all_alignment:
-                for pawn in alignment:
-                    self.state[pawn[0]][pawn[1]] = 3
-                self.trace_grille()
-            return True
+        for alignment in all_alignment:
+            for pawn in alignment:
+                self.grid.grid[pawn[0]][pawn[1]] = 2
+        self.trace_grille()
+        return True
 
 
 class PowerFourGUI(tkinter.Tk):
@@ -384,16 +450,18 @@ class PowerFourGUI(tkinter.Tk):
         # self.master.title(" Game of power four - Two Player")
 
         self.geometry("400x300")
-        self.title(" Game of power four - Two Player")
+        self.title("Game of power four - Two Player")
 
-        self.jeu = Panel(self)
+        self.jeu = GridGUI(self)
 
         menu_config = [
             ('File', [
                 ('Options', self.options),
+                None,
                 ('Undo', self.jeu.undo),
                 ('Restart', self.jeu.reset),
-                ('Quit', self.quit)
+                None,
+                ('Quit', self.destroy)
             ]),
             ('Help', [
                 ('Principle of the game', self.principle),
@@ -414,13 +482,13 @@ class PowerFourGUI(tkinter.Tk):
         opt = Toplevel(self)
         cur_l = Scale(opt, length=200, label="Number of lines :",
                       orient=tkinter.HORIZONTAL,
-                      from_=1, to=12, command=self.update_nb_cols)
-        cur_l.set(self.jeu.n_row)  # initial position of the cursor
+                      from_=1, to=12, command=self.update_nb_rows)
+        cur_l.set(self.jeu.grid.n_rows)  # initial position of the cursor
         cur_l.pack()
         cur_h = Scale(opt, length=200, label="Number of columns :",
                       orient=tkinter.HORIZONTAL,
                       from_=1, to=12, command=self.update_nb_cols)
-        cur_h.set(self.jeu.n_col)
+        cur_h.set(self.jeu.grid.n_cols)
         cur_h.pack()
 
     def update_nb_cols(self, n):
