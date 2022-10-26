2a62b1ab53083510f77ceee99532b901e49bd240
Darkduv <40823426+Darkduv@users.noreply.github.com>
2022-10-26 17:18:42 +0200

    change implementation of the gui widgets to handle the resizing, and a lot of refactoring

--- main.py
+++ main.py
@@ -188,7 +188,7 @@ class PowerFour(GameNPlayer):
         player = self.player
         alignments = self.search_alignment(player)
         return player if alignments else -1
-        
+
     def search_alignment(self, player: int = None):
         orientation = [[0, 1], [1, 0], [1, 1], [1, -1]]
         if player is None:
@@ -218,12 +218,13 @@ class PowerFour(GameNPlayer):
     def __str__(self):
         s = ""
         s += f"Power 4, turn={self.turn}, player={self.player}\n"
-        s += "="*(2*self.n_cols + 1)+"\n"
+        s += "=" * (2 * self.n_cols + 1) + "\n"
         for row in range(self.n_rows):
-            s += f"|{' '.join('.OX'[player+1] for player in self.grid[row])}|\n"
-        s += "="*(2*self.n_cols + 1)
+            s2 = ' '.join('.OX'[player + 1] for player in self.grid[row])
+            s += f"|{s2}|\n"
+        s += "=" * (2 * self.n_cols + 1)
         return s
-    
+
     def can_play(self) -> bool:
         for col in range(self.n_cols):
             if self.grid[0][col] != self._not_a_player:
@@ -249,6 +250,9 @@ class GridGUI(Frame):
     """GUI for the Grid of Power4
     TODO : force first player color ?
             #  (be careful with A.I. definition)"""
+    _colors = ["red", "yellow"]
+    _colors_all = []
+    _names = ["Red", "Yellow"]
 
     def __init__(self, root):
         # Red = 0 and Yellow = 1
@@ -261,41 +265,56 @@ class GridGUI(Frame):
         # Link of the event <resize> with an adapted manager :
         self.bind("<Configure>", self.rescale)
         # Canvas :
-        self.can = Canvas(self, bg="dark blue", borderwidth=0,
-                          highlightthickness=1, highlightbackground="white")
+        self.can_grid = Canvas(self, bg="dark blue", borderwidth=0,
+                               highlightthickness=1,
+                               highlightbackground="white")
         # Link of the event <click of the mouse> with its manager :
-        self.can.bind("<Button-1>", self.click)
-        self.can.pack(side=tkinter.LEFT)
-        self.can_bis = Canvas(self, bg="white", borderwidth=0,
-                              highlightthickness=1, highlightbackground="white")
-        self.turn = self.can_bis.create_text(self.can_bis.winfo_width() / 2,
-                                             self.can_bis.winfo_height() / 3,
-                                             text="Yellow's\n turn",
-                                             font="Helvetica 18 bold")
-        x1 = self.can_bis.winfo_width() / 3
-        y = self.can_bis.winfo_height() * 2 / 3
-        y1 = y - x1
-        x2 = x1 * 2
-        y2 = y + x1
-        self.turn_bis = self.can_bis.create_oval(x1, y1, x2, y2, outline="grey",
-                                                 width=1, fill="yellow")
-        # self.can_bis = Label(text="Red's\n turn")
-        self.can_bis.pack(side=tkinter.RIGHT, expand=tkinter.YES)
+        self.can_grid.bind("<Button-1>", self.click)
+        self.can_grid.pack(side=tkinter.LEFT)
+
+        self.side_panel = Canvas(self, bg="white", borderwidth=2,
+                                 highlightthickness=0,
+                                 highlightbackground="white")
+        self.side_panel.pack(side=tkinter.RIGHT,
+                             expand=tkinter.YES, fill=tkinter.BOTH)
+        side_canvas = tkinter.Canvas(self.side_panel,
+                                     bg="white", borderwidth=0,
+                                     highlightthickness=0,
+                                     width=80, height=80)
+        side_canvas.place(relx=0.5, rely=0.666, anchor="center")
+        circle = side_canvas.create_oval(0, 0, 79, 79,
+                                         outline="grey",
+                                         width=1,  fill="red")
+        _circle_player = (side_canvas, circle)
+
+        def _config_circle(fill):
+            _circle_player[0].itemconfig(_circle_player[1], fill=fill)
+        self._config_message_circle = _config_circle
+        self.message = tkinter.Label(self.side_panel, text="Yellow's\n turn",
+                                     font="Helvetica 18 bold",
+                                     bg="white",
+                                     borderwidth=0)
+        self.message.place(relx=0.5, rely=0.35, anchor="center")
+        self.side_panel.pack(side=tkinter.RIGHT,
+                             expand=tkinter.YES, fill=tkinter.BOTH)
         # construction of a list of lists
         self.grid = PowerFour(6, 7)  # default values : 6*7
         self.history = SimpleHistoric()
         self.width, self.height = 2, 2
         self.cote = 0
         self.win = False
-        self.init_jeu()
+        self.init_game()
+        self.init_gui()
 
-    def init_jeu(self, state: PowerFour = None):
+    def init_game(self, state: PowerFour = None):
         """Initialisation of the list which remember the state of the game"""
         self.win = False
         if state is None:
             self.grid.init_game()
         else:
             self.grid = state
+
+    def init_gui(self):
         self.set_side()
         self.trace_grille()
 
@@ -307,65 +326,54 @@ class GridGUI(Frame):
         # the side of a case would be the smallest of the two dimensions :
         self.cote = min(l_max, h_max)
 
-    def rescale(self, event):
+    def rescale(self, event: tkinter.Event = None):
         """Operations made at each rescaling"""
         # the properties which are linked to the event of reconfiguration
         # contain all the new sizes of the panel :
-        self.width, self.height = event.width - 4, event.height - 4
-        # The subtraction of 4 pixels allows to compensate the width
-        # of the 'highlight bordure' rolling the canvas
+        highlight_bordure = 1
+        self.width, self.height = self.winfo_width() - highlight_bordure * 2, \
+            self.winfo_height() - highlight_bordure * 2
         self.set_side()
         self.trace_grille()
+        if event:
+            pass
 
     def trace_grille(self):
         """Layout of the grid, in function of dimensions and options"""
-        # -> establishment of new dimensions for the canvas :
+        # -> determining new dimensions for the canvas :
         wide, high = self.cote * self.grid.n_cols, self.cote * self.grid.n_rows
-        self.can.configure(width=wide, height=high)
+        self.can_grid.configure(width=wide, height=high)
         # Layout of the grid:
-        self.can.delete(tkinter.ALL)  # erasing of the past Layouts
+        self.can_grid.delete(tkinter.ALL)  # erasing of the past Layouts
         # Layout of all the pawns,
         # white or black according to the state of the game :
-        for l in range(self.grid.n_rows):
-            for c in range(self.grid.n_cols):
-                x1 = c * self.cote + 3  # size of pawns =
-                x2 = (c + 1) * self.cote - 3  # size of the case -6
-                y1 = l * self.cote + 3  #
-                y2 = (l + 1) * self.cote - 3
-                color = ["white", "red", "yellow", "black"][self.grid.grid[l][c]+1]
-                self.can.create_oval(x1, y1, x2, y2, outline="grey",
-                                     width=1, fill=color)
-        self.can_bis.configure(width=self.width - wide, height=self.height)
-        self.can_bis.delete(self.turn, self.turn_bis)
-        self.turn = \
-            self.can_bis.create_text(
-                self.can_bis.winfo_width() / 2,
-                self.can_bis.winfo_height() / 3,
-                text=f"{['Red', 'Yellow'][self.grid.player]}'s"
-                     f"\n turn",
-                font="Helvetica 18 bold")
-        x = self.can_bis.winfo_width() / 3
-        y = self.can_bis.winfo_height() / 3
-        r = min([x, y, 40])
-        y1 = y * 2 - r
-        x1 = 3 * x / 2 - r
-        x2 = 3 * x / 2 + r
-        y2 = y * 2 + r
-        self.turn_bis\
-            = self.can_bis.create_oval(x1, y1, x2, y2, outline="grey", width=1,
-                                       fill=["red", "yellow"][self.grid.player])
+        for row in range(self.grid.n_rows):
+            for col in range(self.grid.n_cols):
+                x1 = col * self.cote + 3  # size of pawns =
+                x2 = (col + 1) * self.cote - 3  # size of the case -6
+                y1 = row * self.cote + 3  #
+                y2 = (row + 1) * self.cote - 3
+                color = ["white", "red", "yellow", "black"][
+                    self.grid.grid[row][col] + 1]
+                self.can_grid.create_oval(x1, y1, x2, y2, outline="grey",
+                                          width=1, fill=color)
+        # self.can_grid.update()
+        self.message.config(text=f"{['Red', 'Yellow'][self.grid.player]}'s"
+                                 f"\n turn")
+        self._config_message_circle(["red", "yellow"][self.grid.player])
+
+        # self.side_panel.update()
 
     def click(self, event):
         """Management of the mouse click : return the pawns"""
         # We start to determinate the line and the columns :
-        # row, int(event.y / self.cote),
         if self.win:
             return
+        # row, int(event.y / self.cote),
         col = int(event.x / self.cote)
         if not 0 <= col < self.grid.n_cols:
             raise InvalidActionError(
                 "Click in the grid to make a valid action")
-            # return
 
         player = self.grid.player
         self.history.save_new(self.grid.copy())
@@ -373,7 +381,7 @@ class GridGUI(Frame):
         # maximal width and height possibles for the cases :
         self.set_side()
         wide, high = self.cote * self.grid.n_cols, self.cote * self.grid.n_rows
-        self.can.configure(width=wide, height=high)
+        self.can_grid.configure(width=wide, height=high)
         row = 0
         x1 = self.cote * col + 3
         x2 = self.cote * (col + 1) - 3
@@ -381,39 +389,36 @@ class GridGUI(Frame):
         while row < self.grid.n_rows:
             y1 = row * self.cote + 3
             y2 = (row + 1) * self.cote - 3
-            self.can.create_oval(x1, y1 - self.cote, x2, y2 - self.cote,
-                                 outline="grey", width=1, fill="white")
+            self.can_grid.create_oval(x1, y1 - self.cote, x2, y2 - self.cote,
+                                      outline="grey", width=1, fill="white")
             self.update()
-            self.can.create_oval(x1, y1, x2, y2, outline="grey",
-                                 width=1, fill=color_player)
+            self.can_grid.create_oval(x1, y1, x2, y2, outline="grey",
+                                      width=1, fill=color_player)
             self.update()
-            self.can.after(100)
+            self.can_grid.after(100)
             if self.grid.grid[row][col] != -1:
                 break
             row += 1
 
         if win == -1:
-            # self.can_bis.destroy()
-            # self.can_bis = Label(text=["Red", "Yellow"][self.player]+\
+            # self.side_panel.destroy()
+            # self.side_panel = Label(text=["Red", "Yellow"][self.player]+\
             # "'s\n turn", font="Helvetica 15 bold")
-            self.can_bis.itemconfig(
-                self.turn,
+            self.message.config(
                 text=f"{['Red', 'Yellow'][self.grid.player]}'s\n turn")
-            self.can_bis.itemconfig(
-                self.turn_bis, fill=["red", "yellow"][self.grid.player])
 
-            self.can_bis.pack(side=tkinter.RIGHT)
-            self.can_bis.update()
+            self._config_message_circle(["red", "yellow"][self.grid.player])
+
+            self.side_panel.pack(side=tkinter.RIGHT)
+            self.side_panel.update()
             self.trace_grille()
         else:
             self.win = True
             self.display_victory(player)
-            self.update()
 
-            self.can_bis.itemconfig(
-                self.turn,
+            self.message.config(
                 text=f"{['Red', 'Yellow'][player]}\n wins !!")
-            self.can_bis.delete(self.turn_bis)
+            self._config_message_circle(["red", "yellow"][player])
 
     def undo(self, event=None):
         last_state = self.history.undo()
@@ -424,9 +429,9 @@ class GridGUI(Frame):
         self.trace_grille()
 
     def reset(self, event=None):
-        self.init_jeu()
+        self.init_game()
         self.win = False
-        self.trace_grille()
+        self.init_gui()
         if event:
             pass
 
@@ -446,20 +451,18 @@ class PowerFourGUI(tkinter.Tk):
 
     def __init__(self):
         super().__init__()
-        # self.master.geometry("400x300")
-        # self.master.title(" Game of power four - Two Player")
 
         self.geometry("400x300")
         self.title("Game of power four - Two Player")
 
-        self.jeu = GridGUI(self)
+        self.game = GridGUI(self)
 
         menu_config = [
             ('File', [
                 ('Options', self.options),
                 None,
-                ('Undo', self.jeu.undo),
-                ('Restart', self.jeu.reset),
+                ('Undo', self.game.undo),
+                ('Restart', self.game.reset),
                 None,
                 ('Quit', self.destroy)
             ]),
@@ -472,10 +475,17 @@ class PowerFourGUI(tkinter.Tk):
         self.m_bar = MenuBar(self)
         self.m_bar.config_menu(menu_config)
 
-        self.jeu.pack(expand=tkinter.YES, fill=tkinter.BOTH, padx=8, pady=8)
+        self.game.pack(expand=tkinter.YES, fill=tkinter.BOTH, padx=8, pady=8)
 
-        self.bind("<Command-z>", self.jeu.undo)
-        self.bind("<Command-r>", self.jeu.reset)
+        self.bind("<Command-z>", self.game.undo)
+        self.bind("<Command-r>", self.game.reset)
+
+        children = self.winfo_children()
+        while children:
+            child = children.pop()
+            if isinstance(child, tkinter.Menu):
+                child.config(font=(None, 12))
+                children.extend(child.winfo_children())
 
     def options(self):
         """Choice of the number of lines and columns for the grid"""
@@ -483,31 +493,33 @@ class PowerFourGUI(tkinter.Tk):
         cur_l = Scale(opt, length=200, label="Number of lines :",
                       orient=tkinter.HORIZONTAL,
                       from_=1, to=12, command=self.update_nb_rows)
-        cur_l.set(self.jeu.grid.n_rows)  # initial position of the cursor
+        cur_l.set(self.game.grid.n_rows)  # initial position of the cursor
         cur_l.pack()
         cur_h = Scale(opt, length=200, label="Number of columns :",
                       orient=tkinter.HORIZONTAL,
                       from_=1, to=12, command=self.update_nb_cols)
-        cur_h.set(self.jeu.grid.n_cols)
+        cur_h.set(self.game.grid.n_cols)
         cur_h.pack()
 
     def update_nb_cols(self, n):
         """Updates the number of columns."""
-        self.jeu.n_col = int(n)
-        self.jeu.init_jeu()
+        self.game.grid.n_cols = int(n)
+        self.game.init_game()
+        self.game.rescale()
 
     def update_nb_rows(self, n):
         """Updates the number of rows."""
-        self.jeu.n_row = int(n)
-        self.jeu.init_jeu()
+        self.game.grid.n_rows = int(n)
+        self.game.init_game()
+        self.game.rescale()
 
     # def redo(self):
-    #     if self.jeu.coup < len(self.jeu.game):
-    #         self.jeu.coup += 1
-    #         game = self.jeu.game[self.jeu.coup]
-    #         self.jeu.init_state()
-    #         self.jeu.state = game.copy()
-    #     self.jeu.trace_grille()
+    #     if self.game.coup < len(self.game.game):
+    #         self.game.coup += 1
+    #         game = self.game.game[self.game.coup]
+    #         self.game.init_state()
+    #         self.game.state = game.copy()
+    #     self.game.trace_grille()
 
     def principle(self):
         """window-message containing
@@ -516,14 +528,14 @@ class PowerFourGUI(tkinter.Tk):
         Message(msg, bg="navy", fg="ivory", width=400,
                 font="Helvetica 10 bold",
                 text="You have to manage alone!!!!! \n"
-                     "Réf : Max&Cie, 2017") \
+                     "References : Max&Cie, 2017") \
             .pack(padx=10, pady=10)
 
     def about(self):
         """window-message indicating the author and the type of licence"""
         msg = Toplevel(self)
         Message(msg, width=200, aspect=100, justify=tkinter.CENTER,
-                text="Jeu de Power_Four\n\n by M.Duvillard.\n"
+                text="Game of Connect Four\n\n by M.Duvillard.\n"
                      "Licence = ???").pack(padx=10, pady=10)
 
 
